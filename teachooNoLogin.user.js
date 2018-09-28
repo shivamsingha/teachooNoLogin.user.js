8f189034a7a9371b4d460f96e0a357be18456500
Shivam <shivamsingha@pm.me>
2018-09-28 23:35:28 +0530

    Update teachooNoLogin.user.js

--- teachooNoLogin.user.js
+++ teachooNoLogin.user.js
@@ -3,8 +3,10 @@
 // @namespace    teachooNoLogin
 // @version      1.0
 // @license      MPL-2.0
-// @description  teachoo fix
+// @description  Use teachoo.com without logging in or signing up. It just removes the annoying overlay elements. Nothing flashy.
 // @author       shivamsingha
+// @icon         https://raw.githubusercontent.com/shivamsingha/teachooNoLogin.user.js/master/teachoo_icon.png
+// @homepage     https://github.com/shivamsingha/teachooNoLogin.user.js
 // @supportURL   https://github.com/shivamsingha/teachooNoLogin.user.js/issues
 // @updateURL    https://github.com/shivamsingha/teachooNoLogin.user.js/raw/master/teachooNoLogin.user.js
 // @downloadURL  https://github.com/shivamsingha/teachooNoLogin.user.js/raw/master/teachooNoLogin.user.js
