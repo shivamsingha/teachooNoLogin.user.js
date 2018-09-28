6ed38c02acd19fae5cdaf34e6be0c9cdd6f21b6d
shivamsingha <shivamsingha@pm.me>
2018-09-28 23:07:49 +0530

    Update teachooNoLogin.user.js

--- teachooNoLogin.user.js
+++ teachooNoLogin.user.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         Teachoo fix
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    teachooNoLogin
+// @version      1.0
+// @license      MPL-2.0
 // @description  teachoo fix
-// @author       You
+// @author       shivamsingha
+// @supportURL   https://github.com/shivamsingha/teachooNoLogin.user.js/issues
+// @updateURL    https://github.com/shivamsingha/teachooNoLogin.user.js/raw/master/teachooNoLogin.user.js
+// @downloadURL  https://github.com/shivamsingha/teachooNoLogin.user.js/raw/master/teachooNoLogin.user.js
 // @match        *://*.teachoo.com/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.3.1.slim.min.js
