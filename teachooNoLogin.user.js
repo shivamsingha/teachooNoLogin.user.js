7eca08ca9375be0c1a49cec6f722f5e4acb58051
Shivam <shivamsingha@pm.me>
2018-10-21 21:32:33 +0530

    using jsdelivr cdn

--- teachooNoLogin.user.js
+++ teachooNoLogin.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         Teachoo fix
 // @namespace    teachooNoLogin
-// @version      1.2.0
+// @version      1.2.1
 // @license      MPL-2.0
 // @description  Use teachoo.com without logging in or signing up. It just removes the annoying overlay elements. Nothing flashy.
 // @author       shivamsingha
-// @icon         https://raw.githubusercontent.com/shivamsingha/teachooNoLogin.user.js/master/teachoo_icon.png
+// @icon         https://cdn.jsdelivr.net/gh/shivamsingha/teachooNoLogin.user.js/teachoo_icon.png
 // @homepage     https://github.com/shivamsingha/teachooNoLogin.user.js
 // @supportURL   https://github.com/shivamsingha/teachooNoLogin.user.js/issues
-// @updateURL    https://github.com/shivamsingha/teachooNoLogin.user.js/raw/master/teachooNoLogin.user.js
-// @downloadURL  https://github.com/shivamsingha/teachooNoLogin.user.js/raw/master/teachooNoLogin.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/shivamsingha/teachooNoLogin.user.js/teachooNoLogin.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/shivamsingha/teachooNoLogin.user.js/teachooNoLogin.user.js
 // @match        *://*.teachoo.com/*
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.3.1.slim.min.js
+// @require      https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.slim.min.js
 // @run-at       document-idle
 // ==/UserScript==
 
